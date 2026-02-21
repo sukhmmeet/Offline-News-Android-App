d00e21d8e0b52230bf601a9e6113403f28a56ac8
Sukhmeet Singh Dhaliwal <144631710+sukhmmeet@users.noreply.github.com>
2026-02-21 19:34:15 +0530

    Update API key placeholder in api_interface.kt

--- app/src/main/java/com/dhaliwal/offlinenewsapp/API_Files/api_interface.kt
+++ app/src/main/java/com/dhaliwal/offlinenewsapp/API_Files/api_interface.kt
@@ -6,12 +6,11 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-// https://gnews.io/api/v4/top-headlines?category=general&lang=en&apikey=dde4bd86f3c0ca2960687db96364f0ff
 interface api_interface {
     @GET("top-headlines")
     suspend fun getData(
         @Query("category") category : String = "general",
         @Query("lang") lang : String = "en",
-        @Query("apikey") key : String = "dde4bd86f3c0ca2960687db96364f0ff"
+        @Query("apikey") key : String = "Use your key"
     ) : Response<responseData>
 }
